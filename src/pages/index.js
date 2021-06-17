b02d0b470eff8b11692803e8a8d2466b87bc8543
hud <hudwahab@gmail.com>
2021-06-17 10:08:10 -0600

    optimized images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
-import { Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import Layout from "../components/Layout"
 import * as styles from '../styles/home.module.css'
+import Img from 'gatsby-image'
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data)
+
   return (
     <Layout>
       <section className={styles.header}>
@@ -13,8 +16,20 @@ export default function Home() {
           <p>powered by Cardano blockchain technologies</p>
           <Link className={styles.btn} to="/projects">Explore collectibles</Link>
         </div>
-        <img src="/banner.png" alt="site banner" style={{ maxWidth: '100%' }}/>
+        <Img fluid={data.file.childImageSharp.fluid} />
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+query Banner {
+  file(relativePath: {eq: "banner.png"}) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`
